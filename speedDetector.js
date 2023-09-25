9e6a7f3ef8296d6152a4cc2f8244b55ba6177dc8
rasmi <rasminoel@gmail.com>
2023-09-25 12:42:31 +0300

    Made final changes on the js files, it Works!!

--- speedDetector.js
+++ speedDetector.js
@@ -1,21 +1,22 @@
 
 //define speed using const and making it a number
 let speedLimit = 70;
-let demeritPoints = (Math.floor((speed - speedLimit) / 5));
 
+//this function obtains the speed and returns a value/ string based on that speed.
 function speedCheck(speed) {
-    
-    if (speed < speedLimit) {        //if the speed is less than the speed limit then a value of 'OK' is returned
+    let demeritPoints = ((speed - speedLimit) / 5);
+    if (speed < 71) {
         return 'OK';
     }
-    else if (speed > 70 && demeritPoints < 12) {                  //if the speed is above the speed limit, we find the difference and devide by 5 to obtain the demeritpoints
+    else if (70 < speed && speed < 130) {
         return `Points: ${demeritPoints}`;
     }
-    else {
-        return 'License Suspended';           //if the demeritpoints is above 12, a text of 'License Suspended' is returned.
+    else if (speed > 129) {
+        return 'License Suspended';
     }
 }
 
+//run this to output values in the UI
 function onclickCheckSpeed() {
     let speed = document.getElementById("speedI").value;
     let checkResult = speedCheck(speed);
